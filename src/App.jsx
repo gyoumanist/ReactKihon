e5a7c7e2dbd41d9c4bd3e677347bf3f0273b69b1
gyoumanist <93202305+gyoumanist@users.noreply.github.com>
2022-03-14 20:10:10 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ColofulMessage from "./components/ColorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColofulMessage color="blue" message="お元気ですか？" />
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
