920a26e42346e8b5eadd8dc28df8e6740a1d8225
gyoumanist <93202305+gyoumanist@users.noreply.github.com>
2022-03-17 22:47:54 +0900

    基本講座完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -15,9 +30,9 @@ const App = () => {
       <ColofulMessage color="pink">元気です！</ColofulMessage>
       <button onClick={onClickCountUp}>カウントアップ！</button>
       <br />
-      <button>on/off</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
-      <p>(;´･ω･)</p>
+      {faceShowFlag && <p>(;´･ω･)</p>}
     </>
   );
 };
